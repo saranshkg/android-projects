60850945fec6be021d14ea9a54578674dd66905c
Saransh Gupta <saranshkg@gmail.com>
2020-07-10 20:08:45 +0530

    Classic Snake - Chapter 14 complete

---
+++ gtest/core/src/com/gtest/game/camera/AndroidCamera.java
@@ -0,0 +1,12 @@
+package com.gtest.game.camera;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class AndroidCamera extends OrthographicCamera {
+    public AndroidCamera(float width, float height) {
+
+        super((float)width, (float)height);
+        this.position.x = (float)(width / 2);
+        this.position.y = (float)(height / 2);
+    }
+}
